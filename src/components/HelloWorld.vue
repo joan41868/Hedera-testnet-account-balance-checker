232319a97a0cac5f0521f03f93f9386d28e5191b
Yoan Sredkov <yoansredkov@gmail.com>
2022-02-02 11:17:19 +0200

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="hello bg-dark text-white" style="width: 60%; margin: auto;">
+		<h3 class="mb-3">This app operates on TESTNET.</h3>
+		<h4 class="text-info">Account ID: {{ accountId }}</h4>
+		<h5 class="text-info">Hbar balance {{ hbarBalance }}</h5>
+		<div class="token-balances text-info" v-if="tokenBalances.length > 0">
+			<h5>Token balances:</h5>
+			<h6 v-for="tokenBal in tokenBalances" v-bind:key="tokenBal">
+				{{tokenBal}}
+			</h6>
+		</div>
+		<p v-else class="text-warning">No HTS token balance found.</p>
+		<div class="other-account-check">
+			<h5>Check another account:</h5>
+			<span style="display: flex; flex-direction: row; justify-content: space-between; width: 40%; margin: auto;">
+				<b-input v-model="otherAccountId"/>
+				<b-button variant="outline-info" class="submitButton" @click="loadAnotherAccount()">Check</b-button>
+			</span>
+		</div>
+	</div>
 </template>
 
 <script>
+import {AccountBalanceQuery, Client, HbarUnit} from "@hashgraph/sdk";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+	name: 'HelloWorld',
+	props: {
+		accountId: String
+	},
+	data() {
+		return {
+			hbarBalance: '',
+			tokenBalances: [],
+			client: Client,
+
+			otherAccountId: ''
+		}
+	},
+	async mounted() {
+		await this.loadAccountBalance();
+	},
+	methods:{
+		async loadAccountBalance() {
+			this.client = Client.forTestnet();
+			const balance = await new AccountBalanceQuery()
+				.setAccountId(this.accountId)
+				.execute(this.client);
+			this.hbarBalance = balance.hbars.toString(HbarUnit.Hbar);
+			this.tokenBalances = balance.tokens;
+			console.log(this.tokenBalances);
+		},
+
+		async loadAnotherAccount() {
+			this.accountId = this.otherAccountId;
+			await this.loadAccountBalance();
+		}
+	}
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+	color: #42b983;
+}
+input{
+	margin-right: 5px;
 }
 </style>
